0ba952db845183756dbe10a369292748623f49e2
Sorck <colonel.sorck@gmail.com>
2013-02-06 18:37:36 -0500

    Basic board index controller structure.

--- ForumModule.php
+++ ForumModule.php
@@ -23,13 +23,21 @@ class ForumModule extends Module
 	{
 		// Just to be on the safe side. While these don't hurt us, we don't like 'em either.
 		if (isset($_REQUEST['ssi_theme']) && (int) $_REQUEST['ssi_theme'] == (int) $GLOBALS['ssi_theme'])
+        {
 			die('Hacking attempt...');
+        }
 		elseif (isset($_COOKIE['ssi_theme']) && (int) $_COOKIE['ssi_theme'] == (int) $GLOBALS['ssi_theme'])
+        {
 			die('Hacking attempt...');
+        }
 		elseif (isset($_REQUEST['ssi_layers'], $ssi_layers) && (@get_magic_quotes_gpc() ? stripslashes($_REQUEST['ssi_layers']) : $_REQUEST['ssi_layers']) == $GLOBALS['ssi_layers'])
+        {
 			die('Hacking attempt...');
-		if (isset($_REQUEST['context']))
+		}
+        if (isset($_REQUEST['context']))
+        {
 			die('Hacking attempt...');
+        }
 
 		parent::load();
 	}
