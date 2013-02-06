59308d8fae0d578c11f93de27dfc032593018d0a
Sorck <colonel.sorck@gmail.com>
2013-02-06 18:48:17 -0500

    Fleshing out controller files.

---
+++ Controllers/Display.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines http://www.simplemachines.org
+ * @copyright 2013 Simple Machines
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 3.0 Alpha 1
+ */
+
+namespace smCore\Modules\smf\Controllers;
+
+use smCore\Module\Controller;
+
+class Display extends Controller
+{
+    public function main()
+    {
+        // @todo
+    }
+}
