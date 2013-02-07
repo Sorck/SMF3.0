b310daa96528baf9198cbc790b756026d36e42c8
Sorck <colonel.sorck@gmail.com>
2013-02-07 17:13:14 -0500

    Base structure for model classes

---
+++ Models/Category.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines http://www.simplemachines.org
+ * @copyright 2013 Simple Machines
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 3.0 Alpha 1
+ * 
+ * @todo Implement iterator
+ */
+
+namespace smCore\Modules\smf\Storage;
+
+use smCore\Module\Model;
+
+class Category extends Model
+{
+    protected $_data;
+    public $boards;
+    
+    public function setData(array $data)
+    {
+        
+    }
+    public function setRawData(array $data)
+    {
+        
+    }
+    public function save()
+    {
+        $this->module->getStorage('Category')->save($this);
+    }
+}
---
+++ Models/Topic.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines http://www.simplemachines.org
+ * @copyright 2013 Simple Machines
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 3.0 Alpha 1
+ * 
+ * @todo Implement iterator
+ */
+
+namespace smCore\Modules\smf\Storage;
+
+use smCore\Module\Model;
+
+class Topic extends Model
+{
+    protected $_data;
+    
+    public function setData(array $data)
+    {
+        
+    }
+    public function setRawData(array $data)
+    {
+        
+    }
+    public function save()
+    {
+        $this->module->getStorage('Topic')->save($this);
+    }
+}
