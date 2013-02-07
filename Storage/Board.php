f52c9193d9dbea02bf90f5be8be74358b90c4a09
Sorck <colonel.sorck@gmail.com>
2013-02-07 16:59:02 -0500

    Base structure for storage classes

---
+++ Storage/Board.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Simple Machines Forum (SMF)
+ *
+ * @package SMF
+ * @author Simple Machines http://www.simplemachines.org
+ * @copyright 2013 Simple Machines
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 3.0 Alpha 1
+ */
+
+namespace smCore\Modules\smf\Storage;
+
+use smCore\Module\Storage;
+
+class Board extends Storage
+{
+    public function getByID($id_board)
+    {
+        // @todo
+    }
+    public function getByName($name)
+    {
+        // @todo
+    }
+}
